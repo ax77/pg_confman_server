f38ea5787ef5c95d96e575c3dad3b4c09a869ce4
ax77 <mailbox.r8@gmail.com>
2022-09-16 20:53:16 +0600

    Initial commit for a backend.

---
+++ src/main/java/ars/srv/SrvApplication.java
@@ -0,0 +1,13 @@
+package ars.srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvApplication.class, args);
+	}
+
+}
